84fd5362d902cb720a4ba07986f8c1a12a9310cb
ElleryHao <hzk745@126.com>
2018-08-23 14:51:03 +0800

    fix bugs

--- libbitcoin/include/bitcoin/bitcoin/utility/collection.hpp
+++ libbitcoin/include/bitcoin/bitcoin/utility/collection.hpp
@@ -57,7 +57,7 @@ std::vector<Element>& distinct(std::vector<Element>& list);
  * @return             The position or -1 if not found.
  */
 template <typename Pair, typename Key>
-int find_pair_position(const std::vector<const Pair>& list, Key& key);
+int find_pair_position(const std::vector<Pair>& list, Key& key);
 
 /**
  * Find the position of an element in an ordered list.
